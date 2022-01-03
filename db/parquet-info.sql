133b67cdc852cf7bcd61cbcc706837602c610ff9
Arjen P. de Vries <arjen@acm.org>
2022-01-03 18:46:06 +0100

    Choose useful information from everything.

--- db/parquet-info.sql
+++ db/parquet-info.sql
@@ -1,3 +1,3 @@
 -- What's in the Parquet Files?
-SELECT * FROM parquet_metadata('doc-ok.parquet') WHERE row_group_id = 0;
-SELECT * FROM parquet_metadata('doc-ok_field_mapping.parquet');
+SELECT path_in_schema, type, stats_min_value, stats_max_value FROM parquet_metadata('msmarco_doc_00.parquet') WHERE row_group_id = 0;
+SELECT path_in_schema, type, stats_min_value, stats_max_value FROM parquet_metadata('msmarco_doc_00_field_mapping.parquet') WHERE row_group_id = 0;
