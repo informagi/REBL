77268136053e59600d9eb487c1ceebf6b09c8f61
Chris Kamphuis <mail@chriskamphuis.com>
2022-04-25 16:40:37 +0200

    Install syntok through official source.
    The feature requested are implemented in latest syntok version. Other flair removed characters are removed manually as the syntok behaviour on them is expected.

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     author='Chris Kamphuis & Arjen P. de Vries',
     author_email='chris@cs.ru.nl',
     url='https://github.com/informagi/REBL',
-    install_requires=['duckdb', 'pandas', 'pyarrow', 'flair', 'torch>=1.5.0,!=1.8.*',
-                      'syntok @ git+git://github.com/informagi/syntok'],
+    install_requires=['duckdb', 'pandas', 'pyarrow', 'flair', 'torch>=1.5.0,!=1.8.*', 'syntok'],
     packages=find_packages(),
     license='MIT License'
 )
