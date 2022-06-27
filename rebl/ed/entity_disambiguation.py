675159153f627c412348e60f43c0853565e12e38
Chris Kamphuis <mail@chriskamphuis.com>
2022-06-27 20:56:45 +0200

    reset limit to 15k, so we can better inspect the profile

--- rebl/ed/entity_disambiguation.py
+++ rebl/ed/entity_disambiguation.py
@@ -58,7 +58,7 @@ class EntityDisambiguation:
                 yield json_content[self.arguments['identifier']], field, spans, current_text, tags, scores
                 self.stream_parquet_md_file = chain([data], self.stream_parquet_md_file)
             self.docs_done = i + 1
-            if self.docs_done == 5000:
+            if self.docs_done == 15000:
                 import sys
                 sys.exit(0)
             torch.cuda.empty_cache()
