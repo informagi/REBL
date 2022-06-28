af6dc1a22dd28706d2c3dc02ab82e2620584f3c0
Chris Kamphuis <mail@chriskamphuis.com>
2022-06-28 13:45:39 +0200

    remove manual gc

--- rebl/ed/entity_disambiguation.py
+++ rebl/ed/entity_disambiguation.py
@@ -58,8 +58,6 @@ class EntityDisambiguation:
                 yield json_content[self.arguments['identifier']], field, spans, current_text, tags, scores
                 self.stream_parquet_md_file = chain(iter([data]), self.stream_parquet_md_file)
             self.docs_done = i + 1
-            if self.docs_done % 100 == 0:
-                gc.collect()
             if self.docs_done == 20000:
                 import sys
                 sys.exit(0)
