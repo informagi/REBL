83b2202181cd08f1a474da0fd210ea47ee033aa9
Chris Kamphuis <mail@chriskamphuis.com>
2022-08-02 11:16:03 +0200

    Add script to change disambiguated docs to json

--- rebl/utils/__init__.py
+++ rebl/utils/__init__.py
@@ -1,4 +1,5 @@
 from .input_stream_generator import input_stream_gen_lines, stream_parquet_file_per_entry
 from .error_file_to_documents import ErrorFileToDocuments
+from .ed_parquet_to_json_doc import EntityParquetToJSON
 
-__all__ = ['input_stream_gen_lines', 'stream_parquet_file_per_entry', 'ErrorFileToDocuments']
+__all__ = ['input_stream_gen_lines', 'stream_parquet_file_per_entry', 'ErrorFileToDocuments', 'EntityParquetToJSON']
